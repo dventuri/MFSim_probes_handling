aa8faa8e8483f3906e63a08f8667fc1f9632159b
dventuri <diegoventuri@gmail.com>
2021-11-23 16:06:26 -0300

    printing line number and formatting numbers

--- generate_probes.py
+++ generate_probes.py
@@ -26,15 +26,17 @@ xs = [0.1+dx, 0.2, 0.3, 0.4, 0.5-dx]
 
 angle = 0
 for line in lines:
-    line_array = np.zeros((n,3))
+    line_array = np.zeros((n,4))
+    line_array[:,0] = np.arange(1,n+1)
 
     for i in range(n):
-        line_array[i,1] = (points[i]-center)*np.sin(np.deg2rad(angle))+center
-        line_array[i,2] = (points[i]-center)*np.cos(np.deg2rad(angle))+center
+        line_array[i,2] = (points[i]-center)*np.sin(np.deg2rad(angle))+center
+        line_array[i,3] = (points[i]-center)*np.cos(np.deg2rad(angle))+center
 
     for x in xs:
-        line_array[:,0] = x
+        line_array[:,1] = x
         np.savetxt(line+'_'+str(round(x,1))+'.dat', line_array,
+                   fmt=['%i', '%9.7e', '%20.18e', '%20.18e'],
                    header=str(n),
                    comments='')
 
